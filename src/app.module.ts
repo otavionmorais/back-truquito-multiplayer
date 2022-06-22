871886a28f904d6cf8503914ebd7916469fd13cd
Otávio Morais <otavio.n.morais@hotmail.com>
2022-06-21 22:38:57 -0300

    feat: updating dependencies, adding env variables validation and adding prisma dependency

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { EventsModule } from './events/events.module';
 import { HttpModule } from './http/http.module';
 
 @Module({
-  imports: [EventsModule, HttpModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production')
+          .default('development'),
+        PORT: Joi.number().default(3000),
+        WEBSOCKET_PORT: Joi.number().default(3000),
+      }),
+    }),
+    EventsModule,
+    HttpModule,
+  ],
 })
 export class AppModule {}
